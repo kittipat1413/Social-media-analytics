3ee1050fa3be7a9043f6b3f8e5c4f3033217c078
Thaned Thanawadee <talented.ned@gmail.com>
2021-09-17 11:04:12 +0700

    Update datasets and page1 file
    1. Updated new datasets file
    2. Change Layout of page1

--- layouts/page1.py
+++ layouts/page1.py
@@ -17,107 +17,69 @@ df_day_vs_time.set_index(['channel', 'day'], inplace=True)
 df_channel_engagement = pd.read_csv(DATA_PATH.joinpath("df_channel_engagement.csv"))
 df_channel_account_top_engagement = pd.read_csv(DATA_PATH.joinpath("df_channel_account_top_engagement.csv"))
 
-
 layout = html.Div([
-                # First row
-                html.Div([
-                    # First part in row
-                    html.Div([
-                        html.H2("Time VS Day by engagement of each channel"),
 
-                        dcc.Dropdown(
-                            id='channel',
-                            options=[{"value": x, "label": x}
-                                    for x in channel],
-                            value='facebook'
-                        ),
+                        # First row
+                        html.Div([
+                            # First part in row
+                            html.Div([
+                                html.H2("Most popular channel by influencer"),
+                                dcc.Graph(id="graph")
+                                    ], className="six columns"),
 
-                        dcc.Graph(id="graph")
-                    ], className="eight columns"),
+                            # Second part in row
+                            html.Div([
+                                html.H2("Most popular channel by engagement"),
+                                dcc.Graph(id="graph2")
+                                    ], className="six columns"),
 
-                    # Second part in row
-                    html.Div([
-                        html.H2("Ratio of engagement of each channel"),
+                                ], className="row"),
 
-                        html.Br(),
+                        # Second row
+                        html.Div([
+                            
+                            # First part in row
+                            html.Div([
+                                html.H2("Best time to post by channel"),
 
-                        dcc.Graph(id="graph2")
-                    ], className="four columns"),
+                                dcc.Dropdown(
+                                            id='channel',
+                                            options=[{"value": x, "label": x}
+                                                    for x in channel],
+                                            value='facebook'
+                                            ),
 
-                ], className="row"),
+                                dcc.Graph(id="graph3")
+                                    ]),
 
-                # Second row
-                html.Div([
-                    
-                    # First part in row
-                    html.Div([
-                        
-                        html.H2("Top 15 engagement of each platform"),
-
-                        dcc.Dropdown(
-                            id='channel2',
-                            options=[{"value": x, "label": x}
-                                    for x in channel],
-                            value='facebook'
-                        ),
-
-                        dcc.Dropdown(
-                            id='top_filter',
-                            options=[{"value": x, "label": "Top " + str(x) }
-                                    for x in range(5,31,5)],
-                            value='5'
-                        ),
-
-                        dcc.Graph(id="graph3")
-                    ], className="eight columns"),
-
-                    # Second part in row
-                    html.Div([
-                        html.H2("Ratio of engagement of each channel"),
-
-                        html.Br(),
-
-                        dcc.Graph(id="graph4")
-                    ], className="four columns"),
-
-                ], className="row")
-])
+                                ], className="row")
+                ])
 
 @app.callback(
     [Output("graph", "figure"),
     Output("graph2", "figure"),
-    Output("graph3", "figure"),
-    Output("graph4", "figure")], 
-    [Input("channel", "value"),
-    Input("channel2", "value"),
-    Input("top_filter", "value")])
-def change_channel(channel_drop, channel_drop2, top_drop):
-    fig_graph1 = px.imshow(df_day_vs_time.loc[channel_drop],
+    Output("graph3", "figure")], 
+    Input("channel", "value"))
+def change_channel(channel_drop):
+    
+    # define donut color
+    donut_colors=['#104E8B', '#9A32CD', 'lightskyblue', '#B22222']
+
+    fig_donut = go.Figure(data=[go.Pie(labels=df_channel_engagement['channel'], values=df_channel_engagement['account_id'], hole=.5, marker_colors=donut_colors, sort=False)])
+    fig_donut.update_layout(annotations=[dict(text='Channel', x=0.5, y=0.5, font_size=20, showarrow=False)])
+
+
+    fig_donut2 = go.Figure(data=[go.Pie(labels=df_channel_engagement['channel'], values=df_channel_engagement['new_engagement'], hole=.5, marker_colors=donut_colors, sort=False)])
+    fig_donut2.update_layout(annotations=[dict(text='Channel', x=0.5, y=0.5, font_size=20, showarrow=False)])
+
+
+    fig_graph3 = px.imshow(df_day_vs_time.loc[channel_drop],
                 labels=dict(y="Day of Week", x="Time of Day", color="Engagment(%)"),
                 x=df_day_vs_time.columns,
                 y=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
                 color_continuous_scale='blues'
                )
+    fig_graph3.update_layout(xaxis_tickangle=-45)
 
-
-    fig_graph2 = go.Figure(data=[go.Pie(labels=df_channel_engagement['channel'], values=df_channel_engagement['engagement'], hole=.5)])
-    fig_graph2.update_layout(annotations=[dict(text='Channel', x=0.5, y=0.5, font_size=20, showarrow=False)])
-
-
-    fig_graph3 = go.Figure()
-    fig_graph3.add_trace(go.Bar(
-        y=df_channel_account_top_engagement[df_channel_account_top_engagement['channel'] == channel_drop2].tail(int(top_drop))['account_display_name'],
-        x=df_channel_account_top_engagement[df_channel_account_top_engagement['channel'] == channel_drop2].tail(int(top_drop))['engagement'],
-        orientation='h',
-        marker=dict(
-            color='rgba(14, 159, 102, 0.6)',
-            line=dict(color='rgba(14, 159, 102, 1.0)', width=3)
-        )
-    ))
-
-
-    fig_graph4 = go.Figure(data=[go.Pie(labels=df_channel_engagement['channel'], values=df_channel_engagement['engagement'], hole=.5)])
-    fig_graph4.update_layout(annotations=[dict(text='Channel', x=0.5, y=0.5, font_size=20, showarrow=False)])
-
-    return fig_graph1, fig_graph2, fig_graph3, fig_graph4
+    return fig_donut, fig_donut2, fig_graph3
 
