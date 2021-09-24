314c402f389fb7dd05b89d77d4bc98d79b938280
kittipat <kittipat_1413@hotmail.com>
2021-09-24 18:35:37 +0700

    feat: add loading and navbar

--- index.py
+++ index.py
@@ -13,22 +13,22 @@ from layouts import page2
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div([
-        dcc.Link('Page1 |', href='/page1'),
-        dcc.Link(' Page2', href='/page2'),
-    ], className="row"),
-    html.Div(id='page-content')
+        dcc.Link(' Page1 ',id='page1', href='/page1'),
+        dcc.Link(' Page2 ',id='page2', href='/page2'),
+    ], className="topnav"),
+    dcc.Loading(type="default",fullscreen=True, style={'backgroundColor': 'transparent'},children=html.Div(id='page-content'))
 ])
 
 
-@app.callback(Output('page-content', 'children'),
+@app.callback([Output('page-content', 'children'),Output('page1', 'className'),Output('page2', 'className')],
               [Input('url', 'pathname')])
 def display_page(pathname):
     if pathname == '/page1':
-        return page1.layout
+        return page1.layout,"active","" 
     if pathname == '/page2':
-        return page2.layout
+        return page2.layout,"","active"
     else:
-        return page1.layout
+        return page1.layout,"active",""
 
 
 if __name__ == '__main__':
